b5c02def379404bc85cce9b9756c9619136a11e3
Adam Back <adb33@email.arizona.edu>
2014-08-11 15:53:22 -0700

    Passes test. Maybe refactor without method.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,11 +6,17 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) { 
   var output = [];
-  var currentElement = document.body;
-  if(currentElement.classList.contains(className)) {
-    output.push(currentElement); 
-  }
-   return output;
+  var elements = document.getElementsByTagName('*');
+  //all elements
+  for(var i = 0; i < elements.length; i++) {
+    if(elements[i].classList.contains(className)) {
+      output.push(elements[i]);
+    }
+  };
+  
+  var children = elements.childNodes;
+  console.log();
+  return output;
 };
 //Go into document.
 //Find the body.
