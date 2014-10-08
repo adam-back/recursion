ad50205edd726cdf6198b447b64cc0b4bb953f95
Adam Back <adb33@email.arizona.edu>
2014-10-07 21:45:36 -0700

    Beginning on stringify.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,30 +5,6 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
-  var output = [];
-  var recursion = function(element) {
-    if(element.classList) {
-      for(var i = 0; i < element.classList.length; i++) {
-        if(element.classList[i] === className) {
-          output.push(element);
-        }
-      }
-    }
-
-    if(element.childNodes.length > 0) {
-      for(var a = 0; a < element.childNodes.length; a++) {
-        recursion(element.childNodes[a]);
-      }
-    }
-  };
-
-  recursion(document.body);
 
   return output;
 };
-//Go into document.
-//Find the body.
-//Search bpdy for className
-//
-//Search each element for className
-//	if element has className
